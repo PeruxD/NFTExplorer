6ae002cac0ba1afb703ac32b3b309748d8808e21
PeruxD <rezakeaocrew@gmail.com>
2022-11-25 01:18:28 -0500

    Update fetchNFTs.js

--- src/utils/fetchNFTs.js
+++ src/utils/fetchNFTs.js
@@ -1,6 +1,6 @@
 // Go to www.alchemy.com and create an account to grab your own api key!
-const apiKey = "RVakgtAN1cOw89v5ZlSrVJ1jSFQyzRug";
-const endpoint = `https://eth-mainnet.alchemyapi.io/v2/${apiKey}`;
+const { ethers } = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider("http://18.204.219.222:8501/");
 
 export const fetchNFTs = async (owner, contractAddress, setNFTs, retryAttempt) => {
     if (retryAttempt === 5) {
@@ -10,12 +10,12 @@ export const fetchNFTs = async (owner, contractAddress, setNFTs, retryAttempt) =
         let data;
         try {
             if (contractAddress) {
-                data = await fetch(`${endpoint}/getNFTs?owner=${owner}&contractAddresses%5B%5D=${contractAddress}`).then(data => data.json())
+                data = await fetch(`${provider}/getNFTs?owner=${owner}&contractAddresses%5B%5D=${contractAddress}`).then(data => data.json())
             } else {
-                data = await fetch(`${endpoint}/getNFTs?owner=${owner}`).then(data => data.json())
+                data = await fetch(`${provider}/getNFTs?owner=${owner}`).then(data => data.json())
             }
         } catch (e) {
-            fetchNFTs(endpoint, owner, contractAddress, setNFTs, retryAttempt+1)
+            fetchNFTs(provider, owner, contractAddress, setNFTs, retryAttempt+1)
         }
 
         setNFTs(data.ownedNfts)
